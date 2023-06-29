12bc8872b95a10597018e2cd23bd81f99b75e4ab
m4ggiee <121451790+m4ggiee@users.noreply.github.com>
2023-06-29 15:01:59 +0300

    JShw1

---
+++ homework.js
@@ -0,0 +1,59 @@
+// 1
+let age = 17;
+console.log(age);
+
+
+// 2
+let name = "Margo";
+console.log(name);
+
+
+// 3
+let isStudent = "true";
+console.log(isStudent);
+
+
+// 4
+let myString = "Be brave enough to be bad at somthing new";
+console.log(myString);
+
+
+// 5
+let myNumber = 15;
+myNumber += 10;
+console.log(myNumber);
+
+
+// 6
+var myNull = null;
+console.log(myNull);
+
+
+// 7
+let enterName = prompt('Enter your name');
+alert(`Congratulation ${enterName}!`);
+
+
+
+// 8
+if (confirm("якщо хочете отримати вірус підтвердіть нижче)")) {
+    alert("Дякую за підтвердження!");
+  } else {
+    alert("Дію відмінено!");
+}
+
+
+
+
+// 9
+let alt =alert("ПОПЕРЕДЖЕННЯ!!! Дія скоєна вами НЕБЕЗПЕЧНА!");
+if (confirm("Підтвердіть свою дію")) {
+    alert("Дякую за підтвердження!");
+  } else {
+    alert("Дію відмінено!");
+}
+
+
+
+
+
