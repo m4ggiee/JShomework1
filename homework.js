4f1e192c6cc8685f1e2808a3bcf7009824c2ffc2
m4ggiee <121451790+m4ggiee@users.noreply.github.com>
2023-07-07 16:12:09 +0300

    JS2

--- homework.js
+++ homework.js
@@ -1,57 +1,117 @@
-// 1
-let age = 17;
-console.log(age);
+// HOMEWORCK_1
+// // 1
+// let age = 17;
+// console.log(age);
 
+
+// // 2
+// let name = "Margo";
+// console.log(name);
+
+
+// // 3
+// let isStudent = true;
+// console.log(isStudent);
+
+
+// // 4
+// let myString = "Be brave enough to be bad at somthing new";
+// console.log(myString);
+
+
+// // 5
+// let myNumber = 15;
+// myNumber += 10;
+// console.log(myNumber);
+
+
+// // 6
+// var myNull = null;
+// console.log(myNull);
+
+
+// // 7
+// let enterName = prompt('Enter your name');
+// alert(`Congratulation ${enterName}!`);
+
+
+
+// // 8
+// if (confirm("якщо хочете отримати вірус підтвердіть нижче)")) {
+//     alert("Дякую за підтвердження!");
+//   } else {
+//     alert("Дію відмінено!");
+// }
+
+
+
+
+// // 9
+// let alt =alert("ПОПЕРЕДЖЕННЯ!!! Дія скоєна вами НЕБЕЗПЕЧНА!");
+// if (confirm("Підтвердіть свою дію")) {
+//     alert("Дякую за підтвердження!");
+//   } else {
+//     alert("Дію відмінено!");
+// }
+
+
+
+
+// HOMEWORCK_2
+
+// 1
+let C = prompt("введіть температуру:") ;
+C=C*9/5+32
+console.log(C);
 
 // 2
-let name = "Margo";
-console.log(name);
-
+let Days = prompt("Введіть кількість днів:") ;
+Days=Days*24;
+let m = Days*60;
+console.log("Години:", Days, "Хвилини:", m);
 
 // 3
-let isStudent = true;
-console.log(isStudent);
+let health = prompt("Введіть рівень здоров'я:");
+health -= 20
+let Energy = prompt("Введіть рівень Енергії:");
+Energy-=60
+console.log('HP:', health);
+console.log('Energy:', Energy);
 
 
 // 4
-let myString = "Be brave enough to be bad at somthing new";
-console.log(myString);
-
+let check = prompt("Введіть вашу сумму, і отримайтк 10% знижку!");
+check = check-(check*10/100)
+console.log('Сумма для сплачення:', check);
 
 // 5
-let myNumber = 15;
-myNumber += 10;
-console.log(myNumber);
-
+let number = prompt("Введіть число:");
+number=Math.floor(number);
+console.log( number);
 
 // 6
-var myNull = null;
-console.log(myNull);
+let number = prompt("Введіть число:");
+number = parseFloat(number);
+console.log(number);
 
 
 // 7
-let enterName = prompt('Enter your name');
-alert(`Congratulation ${enterName}!`);
-
+let number = prompt("Введіть число:");
+number = parseInt(number);
+console.log(number);
 
 
 // 8
-if (confirm("якщо хочете отримати вірус підтвердіть нижче)")) {
-    alert("Дякую за підтвердження!");
-  } else {
-    alert("Дію відмінено!");
-}
-
-
-
+let number = prompt("Введіть число:");
+number = Math.sqrt(number);
+console.log(number);
 
 // 9
-let alt =alert("ПОПЕРЕДЖЕННЯ!!! Дія скоєна вами НЕБЕЗПЕЧНА!");
-if (confirm("Підтвердіть свою дію")) {
-    alert("Дякую за підтвердження!");
-  } else {
-    alert("Дію відмінено!");
-}
+let number = prompt("Введіть число:");
+console.log(parseInt(number));
+console.log( toString(number));
+
+
 
 
 
